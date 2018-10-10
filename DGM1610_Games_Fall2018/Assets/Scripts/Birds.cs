1939da82499299f02ace6223395793649d700f37
DMutch <goofy.mutch@gmail.com>
2018-10-09 22:25:46 -0600

    Inheritance. Added Birds and Cockatiels script

---
+++ DGM1610_Games_Fall2018/Assets/Scripts/Birds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birds : Animal {
+
+    public int Size;
+    public bool Wings;
+    public Color Feathers;
+    public bool Singing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
