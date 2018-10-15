f96754ae0a961d1a2a9ef53deecbc9dfe79916f2
DMutch <goofy.mutch@gmail.com>
2018-10-15 10:50:22 -0600

    Created Time Of Day Conditionals

--- DGM1610_Games_Fall2018/Assets/Scripts/Conditionals.cs
+++ DGM1610_Games_Fall2018/Assets/Scripts/Conditionals.cs
@@ -28,8 +28,7 @@ public class Conditionals : MonoBehaviour {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	 
 		
-	}
+	 
 }
---
+++ DGM1610_Games_Fall2018/Assets/Scripts/Time.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Time: MonoBehaviour {
+
+    public string TimeOfDay;
+
+	// Use this for initialization
+	void Start () {
+
+        if (TimeOfDay == "Morning")
+            print("TimeOfDay is" + TimeOfDay);
+        else if (TimeOfDay == "Afternoon")
+            print("TimeOfDay is" + TimeOfDay);
+        else if (TimeOfDay == "Evening")
+            print("TimeOfDay is" + TimeOfDay);
+        else if (TimeOfDay == "Night")
+            print("TimeOfDay is" + TimeOfDay);
+
+        else
+            print("TimeOfDay is" + TimeOfDay);
+		
+	}
+	
+	 
+	 
+}
