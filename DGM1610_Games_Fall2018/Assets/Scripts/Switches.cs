9aea05a8e5cc464cc1fa7c7bc96cad98fb66219b
Denise Mutch <goofy.mutch@gmail.com>
2018-10-29 17:13:01 -0600

    Created Switches script

---
+++ DGM1610_Games_Fall2018/Assets/Scripts/Switches.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switches : MonoBehaviour {
+
+	public int num;
+	public string Name;
+
+	// Use this for initialization
+	void Start () {
+
+			//if(num == 10)
+				//print("you picked ten!");
+
+			
+			//else
+				//print("Boo! You didn't pick ten!");
+			
+			//switch(num){
+
+				//case 1:
+					//print("you picked" + num);
+				//break;
+
+				//case 3:
+				//	print("you picked" + num);
+				//break;
+
+			//	case 6:
+				//	print("you picked" + num);
+				//break;
+
+				//case 10:
+					//print("you picked" + num);
+				//break;
+				//default:
+					//print("I don't understand" + num);
+					//break;			
+		//}
+
+		switch(Name){
+			case "Jason":
+				print("Welcom to camp crystal lake" + Name + "- love mother");
+			break;
+
+			case "Michael":
+				print("Welcome to Haddonfield, Ill" + Name);
+			break;
+
+			case "Freddie":
+				print("Welcome to Elm Street" + Name);
+			break;
+
+			case "LeatherFace":
+				print("The stars and stripes are big at night...Deep in the heart of Texas");
+			break;
+
+			default:
+				print("I dont know" + Name);
+			break;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
