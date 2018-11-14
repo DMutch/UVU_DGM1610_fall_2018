a4b14a3fec46cd139ab7728e9b9893dfff284f80
DMutch <goofy.mutch@gmail.com>
2018-11-14 12:51:52 -0700

    Created script for PlayerHealth

---
+++ DGM1610_Games_Fall2018/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+    public string HealthText;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        CurrentHealth = MaxHealth;
+        for (int MaxHealth = 0; 1 < 100; MaxHealth--);
+            
+		
+	}
+	 
+	void Update()
+    { if (CurrentHealth < 0)
+       print("You are Dead" + HealthText);
+
+
+    }
+        }
+    
+
