a851e7219ad19424dcdc1959f8b3260c510bc0ae
DMutch <goofy.mutch@gmail.com>
2018-11-26 13:26:12 -0700

    Fixed errors and commented out player health script

--- DGM1610_Games_Fall2018/Assets/Scripts/PlayerHealth.cs
+++ DGM1610_Games_Fall2018/Assets/Scripts/PlayerHealth.cs
@@ -10,27 +10,27 @@ public class PlayerHealth : MonoBehaviour {
 
 
     // Use this for initialization
-    void Start() {
-        MaxHealth = 100;
-        CurrentHealth = MaxHealth;
-        for (int CurrentHealth = 100; CurrentHealth >= 1; CurrentHealth--);
+   // void Start() {
+//MaxHealth = 100;
+//CurrentHealth = MaxHealth;
+//for (int CurrentHealth = 100; CurrentHealth >= 1; CurrentHealth--);
 
 
-    }
+//}
 
-    void Update() {
-        if (CurrentHealth < 0)
-            CurrentHealth = 0;
-        print("You are Dead" + HealthText);
-    }
+//void Update() {
+      //  if (CurrentHealth < 0)
+          //  CurrentHealth = 0;
+       // print("You are Dead" + HealthText);
+   // }
 
-        public int MaxHealth(int PointsToSubtract) {
-        MaxHealth += PointsToSubtract;
-    }
-    public static void AddPoints(int PointsToAdd)
-    {
-        CurrentHealth += PointsToAdd;
-    }
+       // public int MaxHealth(int PointsToSubtract) {
+        //MaxHealth += PointsToSubtract;
+    //}
+   // public static void AddPoints(int PointsToAdd)
+   // {
+       // CurrentHealth += PointsToAdd;
+    //}
 
 
 }
